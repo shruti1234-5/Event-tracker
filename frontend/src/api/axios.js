f78a2fa9f9a99ae9080cc58930968c53c679488c
shruti1234-5 <shrutisharma620425@gmail.com>
2026-02-23 22:03:25 +0530

    update API base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://event-tracker-1.onrender.com/api',
 });
 
 // Add a request interceptor to include token
